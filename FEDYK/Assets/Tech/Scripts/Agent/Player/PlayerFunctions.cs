c3c689f42b687b3145fcfce5246261b4cef81f47
Benjamin Edwards <benji.robinsclose@btinternet.com>
2018-02-07 13:37:43 +0000

    Updated FPS control
    FPS controller updated to use CharacterController, and the capsule collider has been disabled. Mouselook hopefully in next commit.

---
+++ FEDYK/Assets/Tech/Scripts/Agent/Player/PlayerFunctions.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author - Benjamin Edwards
+ * Date Created - 07/02/2018
+ * Copyright - FEDYK : Games 2018
+ */
+
+public class PlayerFunctions : MonoBehaviour
+{
+    [SerializeField]
+    private int _health = 100;
+    [SerializeField]
+    private int _armour = 50;
+    [SerializeField]
+    private string _username = "Joe Bloggs";
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+    public int Health
+    {
+        get
+        {
+            return _health;
+        }
+        
+        set
+        {
+            _health = value;
+        }
+    }
+
+    public int Armour
+    {
+        get
+        {
+            return _armour;
+        }
+
+        set
+        {
+            _armour = value;
+        }
+    }
+
+    public string UserName
+    {
+        get
+        {
+            return _username;
+        }
+        set
+        {
+            _username = value;
+        }
+    }
+}
