57ad9cabc06f1645513dc1e4559d842a1f6cccdc
Dogelix <yeatmanjake@gmail.com>
2018-02-21 15:04:13 +0000

    Started Main Menu

---
+++ FEDYK/Assets/Tech/Scripts/UI/Quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author - Jake Yeatman
+ * Date - 21/02/218
+ * Copyright - FEDYK : Games 2018
+ */    
+
+public class Quit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
